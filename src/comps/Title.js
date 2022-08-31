1a6aee812608966132629b860f371252950e2237
Johnny Hong <johnny_hong0116@icloud.com>
2022-08-31 12:09:29 +0800

    update 1.0

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -5,8 +5,9 @@ const Title = () => {
     <div className="title">
       <h1><span>P</span>hoto<span>g</span>allery</h1>
       <h2><span>M</span>y <span>P</span>hotos</h2>
-      <p>The artworks by Johnny Hong</p>
-      <p>Website created with Reactjs & Firebase</p>
+      <p>Mainly the artworks by Johnny Hong</p>
+      <p>Anyone can upload their own photos here</p>
+      <p>It is <span>forbidden</span> to download photos on this website</p>
     </div>
   )
 }

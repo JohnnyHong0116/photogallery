6395e447fa0d7a2d96d7eed7994af93e7323946b
Johnny Hong <johnny_hong0116@icloud.com>
2022-08-31 12:26:52 +0800

    adjust title

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -5,7 +5,7 @@ const Title = () => {
     <div className="title">
       <h1><span>P</span>hoto<span>g</span>allery</h1>
       <h2><span>M</span>y <span>P</span>hotos</h2>
-      <p>Mainly the artworks by Johnny Hong</p>
+      <p>Artworks by Johnny Hong [with reproduction paintings]</p>
       <p>Anyone can upload their own photos here</p>
       <p>It is <span>forbidden</span> to download photos on this website</p>
     </div>
